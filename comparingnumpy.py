b68bcc9ce1a591494e246e59e411c7186aad22e4
elusojitimileyin <151373140+elusojitimileyin@users.noreply.github.com>
2023-11-18 16:16:24 +0100

    Update comparingnumpy.py

--- comparingnumpy.py
+++ comparingnumpy.py
@@ -1,11 +1,11 @@
-
+print("Enter three integers")
 number1= int(input("Enter integer:"))
 number2= int(input("Enter integer:"))
 number3= int(input("Enter integer:"))
 
-if number1 == ("largest"):
+if number1 > number2 and number3:
 	print("largest is",number1)
-if number1>number2:
+if number2 >number1 and number3:
 	print("largest is",number2)
-if number1>number3:
+if number3> number1 and number2:
 	print("largest is",number3)
