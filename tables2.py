ab7b925dc67988607afbf68f98d96bf45d4550e0
elusojitimileyin <elusojitimileyin@gmail.com>
2023-11-20 08:34:42 +0100

    assignment

---
+++ tables2.py
@@ -0,0 +1,32 @@
+print("Enter number from 0 to 10 ")
+
+numbero = int(0)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10s%-10s%-10s" % ("Number", "Square", "Cube"))
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
+
+numbero = int(1)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
+
+numbero = int(2)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
+
+numbero = int(3)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
+
+numbero = int(4)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
+
+numbero = int(5)
+squareo = numbero**2
+cubeo = numbero**3
+print(f"%-10d%-10d%-10d" % (numbero, squareo, cubeo))
