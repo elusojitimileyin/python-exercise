817cd21e3e720e0fc9921b6abfc85e51802b4642
elusojitimileyin <elusojitimileyin@gmail.com>
2023-11-23 08:45:53 +0100

    python assignment

--- Heart-RateCalculator.py
+++ Heart-RateCalculator.py
@@ -1,1 +1,9 @@
 print('Heart-Rate Calculator')
+age = int(input())
+maxheartrate = int(220 - age)
+print(maxheartrate,)
+
+minRangefortargetheartrate = 0.50 * maxheartrate
+maxRangefortargetheartrate = 0.85 * maxheartrate
+if (maxheartrate >= 50 and  maxheartrate <= 85):
+	print(f'mim range: {minRangefortargetheartrate}  max range: {maxRangefortargetheartrate}')
